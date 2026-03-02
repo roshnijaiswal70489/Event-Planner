67ee9dfa639f04368a2ecc333bc78fd44a51ff85
Roshni Jaiswal <roshnijaiswal82240@gmail.com>
2026-03-02 11:45:11 +0530

    Modify MongoDB connection string in index.js
    Updated MongoDB connection string to use 'E-planner' database.

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://roshnijaiswal:joJja9i97ekvlmNO@cluster0.zijc2qz.mongodb.net/EventPlanner')
+mongoose.connect('mongodb+srv://roshnijaiswal:joJja9i97ekvlmNO@cluster0.zijc2qz.mongodb.net/E-planner')
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.error('MongoDB Connection Error:', err));
 
@@ -27,4 +27,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
 
